4204e79a35152a6a73e8ef22b592e109003f7a0e
mrhandoko <bambang.handoko12@gmail.com>
2018-02-15 19:39:39 +0700

    adding login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+
+import LoginPage from './pages/LoginPage';
 
 class App extends Component {
   render() {
     return (
-      <div>check this out</div>
+      <Container>
+        <LoginPage />
+      </Container>
     );
   }
 }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div>
+        Halooo Halooo halooo
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
