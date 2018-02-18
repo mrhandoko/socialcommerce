734da3ca039b49031d2ab83af0c863111171656a
mrhandoko <bambank.handoko@yahoo.com>
2018-02-18 17:59:49 +0700

    adding menu sidebar

--- src/base-ui/SidebarMenu.js
+++ src/base-ui/SidebarMenu.js
@@ -1,36 +1,44 @@
 import React from 'react';
-import { Button, Menu, Sidebar, Icon, Segment, Image } from 'semantic-ui-react';
-
-import Paragraph from '../images/paragraph.png';
+import { Menu, Sidebar, Icon } from 'semantic-ui-react'; 
 
 export default class SidebarMenu extends React.Component {
-  // state = { visible: false };
-  // toggleVisibility = () => this.setState({ visible: !this.state.visible });
   render() {
-    const { visible } = this.props;
+    const { visible, children } = this.props;
     return (
       <div>
-        {/* <Button onClick={this.toggleVisibility}>Toggle Visibility</Button> */}
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
-            <Menu.Item name='home'>
-              <Icon name='home' />
-              Home
+        <Sidebar.Pushable style={{ borderRadius: 0, borderWidth: 0 }}>
+          <Sidebar
+            as={Menu}
+            animation="push"
+            width="thin"
+            visible={visible}
+            icon="labeled"
+            vertical
+            inverted
+            style={{ backgroundColor: '#5e5e5e' }}
+          >
+            <Menu.Item name="dashboard">
+              <Icon name="grid layout" /> Dashboard
             </Menu.Item>
-            <Menu.Item name='gamepad'>
-              <Icon name='gamepad' />
-              Games
+            <Menu.Item name="order">
+              <Icon name="conversation" />
+              Order
             </Menu.Item>
-            <Menu.Item name='camera'>
-              <Icon name='camera' />
-              Channels
+            <Menu.Item name="transaction">
+              <Icon name="cart" />
+              Transaction
+            </Menu.Item>
+            <Menu.Item name="finances">
+              <Icon name="calendar outline" />
+              My Finances
+            </Menu.Item>
+            <Menu.Item name="product">
+              <Icon name="heart" />
+              Product
             </Menu.Item>
           </Sidebar>
           <Sidebar.Pusher>
-            <div>
-              <h1>Application Content</h1>
-              <Image src={Paragraph} />
-            </div>
+            {children}
           </Sidebar.Pusher>
         </Sidebar.Pushable>
       </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Image } from 'semantic-ui-react';
 
 import Header from '../base-ui/Header';
 import SecondaryHeader from '../base-ui/SecondaryHeader';
 import SidebarMenu from '../base-ui/SidebarMenu';
+
+import Paragraph from '../images/paragraph.png';
 
 export default class HomePage extends React.Component {
   constructor() {
@@ -20,6 +23,12 @@ export default class HomePage extends React.Component {
         />
         <SidebarMenu
           visible={this.state.visible}
+          children={
+            <div style={{ backgroundColor: 'white', height: 1024 }}>
+              <h1>Application Content</h1>
+              <Image src={Paragraph} />
+            </div>
+          }
         />
       </div>
     );
